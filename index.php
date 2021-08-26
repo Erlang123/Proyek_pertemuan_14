e797709c431217956596ba540912feec38e4874f
Er lang <erlang130@gmail.com>
2021-08-26 18:48:24 +0700

    meta tag fixed

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html>
 	<head>
-		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>Tugas Pertemuan 14</title>
 		<link rel="stylesheet" href="style.css">
 	</head>
