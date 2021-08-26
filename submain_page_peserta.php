77d8116864d30b625f847b0ad784fa136039c2bd
Er lang <erlang130@gmail.com>
2021-08-26 15:02:19 +0700

    buatin sub main baru

---
+++ submain_page_peserta.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Untitled Document</title>
+		<style>
+			body{
+				margin: 0;
+				padding: 0;
+			}
+			a{
+				text-decoration: none;
+				color: black;
+				font-family: MS Reference Sans Serif;
+				font-size: 16pt;
+			}
+			.main-menu{
+				border-bottom:solid 1px black;
+				padding: 10px;
+			}
+			.sub-header{
+				font-family: arial;
+				font-size:20pt;
+				font-weight:bold;
+				padding:2px;
+				margin:2px;
+				
+				
+			}
+		</style>
+	</head>
+	<body>
+		
+		<!-- Container navbar -->
+		<div class="main-menu">
+			<a href="#" class="brand">
+				DIGITAL TALENT
+			</a>
+			
+			<button class="btn-navbar">
+				Sign in
+			</button>
+		</div>
+		<div class="sub-header">
+			Selamat datang Peserta DIGITAL TALENT</div>
+		<hr>
+		<div class="">
+			Anda telah berhasil terdaftar di DIGITAL TALENT
+		</div>
+	</body>
+</html>
