c9540f58def3fc769450ed9eec36698314b09a28
Er lang <erlang130@gmail.com>
2021-08-26 16:48:09 +0700

    Stor file submain admin & update page admin

---
+++ submain_page_admin.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+    <!-- Sidebar -->
+	<container>
+		<!-- Isi dari sidebar -->
+		<h3>Tampil data peserta</h3>
+		<hr>
+		<!-- List data -->
+		<table border="1">
+			<thead>
+				<tr>
+					<td>Nama</td>
+					<td>Jenis Kelamin</td>
+					<td>Agama</td>
+					<td>Asal Sekolah</td>
+					<td>Alamat</td>
+					<td>act</td>
+				</tr>
+			</thead>
+			<tbody>
+				
+			</tbody>
+		</table>
+	</container>
+</body>
+</html>
---
+++ untitled1.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Edit data member</title>
+</head>
+
+<body style="padding: 0;margin: 0;">
+<!-- navbar -->
+	<div style="border-bottom: solid 1px black;">
+		<h2 style="padding: 0;margin: 0px 0px 0 35px; font-weight: unset;">Edit data member</h2>
+	</div>
+<!-- table -->
+	<div style="padding: 20px;width: 98%;">
+		<form action="" style="width: inherit;">
+			<label for="">nama</label><br>
+			<input type="text" style="width: inherit;border-radius: 8px;border:solid 1px;"><br>
+			<label for="">jenis kelamin</label><br>
+			<input type="text" style="width: inherit;border-radius: 8px;border:solid 1px;"><br>
+			<label for="">agama</label><br>
+			<input type="text" style="width: inherit;border-radius: 8px;border:solid 1px;"><br>
+			<label for="">asal</label><br>
+			<input type="text" style="width: inherit;border-radius: 8px;border:solid 1px;"><br>
+			<label for="">nama</label><br>
+			<input type="text" style="width: inherit;border-radius: 8px;border:solid 1px;"><br>
+			<label for="">nama</label><br>
+			<input type="text" style="width: inherit;border-radius: 8px;border:solid 1px;"><br>
+			<div style="display: flex; width: 100%; justify-content: flex-end;">
+				<button style="background: rgba(0,0,255,0.5);border: solid 1px rgba(0,0,0,0.2);padding: 5px 15px;margin: 5px;color:white;">edit</button>
+				<button style="background: rgba(255,255,0,0.6);border: solid 1px rgba(0,0,0,0.2);padding: 5px 15px;margin: 5px;color: darkslategrey;">cancel</button>
+				</div>
+		</form>
+	</div>
+</body>	
+</html>
