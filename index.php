a5fbc61afc0c1c74663a5c305be224839a1e6775
Er lang <erlang130@gmail.com>
2021-08-26 14:20:15 +0700

    halaman utama untuk sign up

--- index.php
+++ index.php
@@ -1,10 +1,69 @@
 <!doctype html>
 <html>
 	<head>
+		<style>
+			content{
+				display:flex;
+				width: 100%;
+				justify-content: center;
+			}
+			.form-wrapper{
+				border:solid 1px rgba(0,0,0,0.1);
+				padding: 20px;
+				/*width: inherit;*/
+			}
+			.form-group{
+				width:100%;
+				height: 100%;
+				background: #ecf0f5;
+			}
+		</style>
 		<meta charset="utf-8">
 		<title>Tugas Pertemuan 14</title>
 	</head>
+	
 	<body>
-		<?php	echo "testing git connection";?>
+		
+		<!-- Content situs -->
+		<content>
+			<div class="form-wrapper">
+				<h2>Login Peserta</h2>
+				<form action="#" method="get">
+					<div class="form-group">
+						<label for="">Nama</label><br>
+						<input type="text" name="pre-nama" required><br>
+						<label for="">Jenis Kelamin</label><br>
+						<input type="radio" name="pre-nama" value="L" required>
+						<label for="">Laki - laki</label><br>
+						
+						<input type="radio" name="pre-nama" value="P" required>
+						<label for="">Perempuan</label><br>
+						
+						<label for="">Agama</label><br>
+						<input type="radio" name="pre-nama" value="P" required>
+						<label for="">Islam</label><br>
+						<input type="radio" name="pre-nama" value="P" required>
+						<label for="">Kristen</label><br>
+						<input type="radio" name="pre-nama" value="P" required>
+						<label for="">Buddha</label><br>
+						<input type="radio" name="pre-nama" value="P" required>
+						<label for="">Hindu</label><br>
+						<input type="radio" name="pre-nama" value="P" required>
+						<label for="">Katholik</label><br>
+						<input type="radio" name="pre-nama" value="P" required>
+						<label for="">Konghucu</label><br>
+						
+						<label for="">Sekolah Asal</label><br>
+						<input type="text" name="pre-nama" required><br>
+						<label for="">Alamat</label><br>
+						<input type="text" name="pre-nama" required><br>
+					 	<input type="submit" value="simpan">
+					</div>
+					
+				</form>
+			</div>
+		</content>
+		
 	</body>
+	
 </html>
