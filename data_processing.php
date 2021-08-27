9b7371e11dd5a1a8b58ff7794dd70e5c1c258a53
Er lang <erlang130@gmail.com>
2021-08-27 15:58:28 +0700

    Re-styling css and building add processing data in data_processing.php

--- data_processing.php
+++ data_processing.php
@@ -1,4 +1,4 @@
-<?php include "conn.php";
+<?php include "conn.php"; session_start();
 $selector = $_GET["selector"];
 
 switch($selector){
@@ -6,8 +6,10 @@ switch($selector){
 		if(isset($_GET["post-nama"], $_GET["post-jenis_kelamin"]) && isset($_GET["post-agama"], $_GET["post-sekolah_asal"]) &&
 		   isset($_GET["post-alamat"])
 		){
-			$query = "('" .  . "', '', '', '', '')";
+			$query = "('" . $jumlah_baris++ . "', '" . $_GET["post-nama"] . "', '" . $_GET["post-jenis_kelamin"] . "', '" . $_GET["post-alamat"] . "', '" . $_GET["post-agama"] . "', '" . $_GET["post-sekolah_asal"] . "')";
 			$result = mysqli_query($conn, "INSERT INTO peserta VALUES" . $query);
+			$_SESSION["isdatauploaded"] = true;
+			header("location:submain_page_peserta.php");
 		}
 	break;
 	default: $_SESSION["isselectorwork"] = false;
